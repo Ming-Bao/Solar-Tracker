45f5e48c12f771f4c3444011c94bc8832fd60597
Ming Bao <mingbao2891@gmail.com>
2023-10-09 17:50:34 +1300

    working Final version. Maybe make more compact

--- projectCode/p4_templ.cpp
+++ projectCode/p4_templ.cpp
@@ -3,7 +3,7 @@
 #include <chrono>
 using namespace std;
 
-//kernals
+//kernals for findng vertical and horizontal edges
 double kernal_x[3][3] = {
 	{-1, 0, 1},
 	{-2, 0, 2},
@@ -21,6 +21,12 @@ struct Edge {
 	vector<int> y;
 };
 
+//struct for passing x and y positions of a single pixel
+struct Pixel {
+	int x;
+	int y;
+};
+
 // you can use that
 struct Orbit {
 	// logged position and time
@@ -28,7 +34,7 @@ struct Orbit {
 	vector<int> y;
 	vector<int> t;
 	int xc,yc,r;  // center and radius
-	int x_sunrise,y_sunrise;
+	int x_sunrise,y_sunrise = 900;
 	double omega = 0.1;
 } orbit;
 
@@ -41,12 +47,7 @@ bool is_red(int row, int col){
 //filters the x and y positions of any red edges and returns them in an Edge struct
 Edge filter_edge(){
 	Edge edge;
-	ImagePPM image1;
-	image1.width = 900;
-	image1.height = 900;
-    image1.n_bytes =  image1.width*image1.height*3;
-    image1.data = new char[image1.n_bytes];
-	
+	 
 	for (int col = 1; col < image.width-1; col++){
 		for (int row = 1; row < image.height-1; row++){
 			//set_pixel(image1, row, col, (int)get_pixel(image, row, col,0), (int)get_pixel(image, row, col,1), (int)get_pixel(image, row, col,2));
@@ -65,14 +66,12 @@ Edge filter_edge(){
 
 				if(r_final > 75){
 					//cout<<"r_final: "<<r_final<<endl;
-					set_pixel(image1, row, col, 0, 255, 0);
 					edge.x.push_back(col);
 					edge.y.push_back(row);
 				}
 			}
 		}
 	}
-	save_bmp_file("test.bmp", image1);
 	return edge;
 }
 
@@ -176,6 +175,8 @@ void calculate_orbit(){
 	orbit.yc = det(yc_top)/det(yc_bottom);
 	orbit.r  = det(r_top)/det(r_bottom);
 	cout<<"xc: "<<orbit.xc<<"yc: "<<orbit.yc<<"r: "<<orbit.r<<endl;
+
+	orbit.x_sunrise = orbit.xc - sqrt(pow(orbit.r, 2) - pow(900-orbit.yc, 2));
 }
 
 //checks if the sun is visiable
@@ -198,6 +199,12 @@ void go_to_sun(){
 	int x_square;
 	int y_square;
 
+	if(y_sun > 900){
+		x_sun = orbit.x_sunrise;
+		y_sun = orbit.y_sunrise;
+	}
+
+
 	get_aim(x_square, y_square);
 
 	double top = y_sun-y_square;
@@ -206,24 +213,117 @@ void go_to_sun(){
 	move_aim(atan2(top, bottom));
 }
 
-//store position for orbit
-void store_orbit_position(int t){
-	vector<int> x;
-	vector<int> y;
+//checks if 2 points are close enough to be part of the circle
+bool close_enough(int x1, int y1, int x2, int y2, int threshold) {
+    int distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    return distance < threshold;
+}
 
-	for (int row = 0; row < image.height; row ++){
-		for (int col = 0; col < image.width; col ++){
-			if (is_red(row, col)){
-				x.push_back(col);
-				y.push_back(row);
+//draws the edges and center of the sun
+void draw_edge_and_center(Pixel pixel, Edge edge, int edge_count){
+	ImagePPM image1;
+	image1.width = 900;
+	image1.height = 900;
+    image1.n_bytes =  image1.width*image1.height*3;
+    image1.data = new char[image1.n_bytes];
+    
+    for (int i = 0; i < edge_count; i++){
+		int x = edge.x.at(i);
+		int y = edge.y.at(i);
+		
+		set_pixel(image1, y, x, 0, 255, 0);
+	}
+	
+	for(int yk = -1; yk <= 1; yk++){
+		for(int xk = -1; xk <= 1; xk++){
+			set_pixel(image1, pixel.y + xk , pixel.x + yk, 255, 0, 0);
+		}
+	}
+	
+    save_bmp_file("test.bmp", image1);
+}
+
+//vote for the center of the circle
+Pixel vote_center(){
+	Edge edge = filter_edge();
+	int vote[900][900];
+	int edge_count = edge.x.size();
+	
+	for (int x = 0; x < 900; x++){
+		for (int y = 0; y < 900; y++){
+			vote[x][y] = 0;
+		}
+	} 
+
+	for (int pix_1 = 0; pix_1 < edge_count; pix_1++){
+		int x1 = edge.x.at(pix_1);
+		int y1 = edge.y.at(pix_1);
+		
+		for (int pix_2 = pix_1; pix_2 < edge_count; pix_2++){
+			//check if the pixel 2 is within 33 pixels from pixel 1
+			if(!close_enough(x1, y1, edge.x.at(pix_2), edge.y.at(pix_2), 33)){continue;}
+			int x2 = edge.x.at(pix_2);
+			int y2 = edge.y.at(pix_2);
+
+			for (int pix_3 = pix_2; pix_3 < edge_count; pix_3++){
+				//check if the pixel 3 is within 33 pixels from pixel 1
+				if(!close_enough(x1, y1, edge.x.at(pix_3), edge.y.at(pix_3), 33)){continue;}
+				int x3 = edge.x.at(pix_3);
+				int y3 = edge.y.at(pix_3);
+
+				//calculating circle center positions
+				double a = (2*x1) - (2*x2);   
+				double b = (2*y1) - (2*y2);
+				double c = pow(x2, 2) - pow(x1, 2) + pow(y2, 2) - pow(y1, 2);
+
+				double d = (2*x1) - (2*x3);
+				double e = (2*y1) - (2*y3);
+				double f = pow(x3, 2) - pow(x1, 2) + pow(y3, 2) - pow(y1, 2);
+
+				int xc = ((-c*e) + (f*b))/((a*e) - (b*d));
+				int yc = ((-a*f) + (c*d))/((a*e) - (b*d));
+
+				if(xc < 900 && xc > 0 && yc < 900 && yc > 0){
+					vote[xc][yc]++;
+				}
 			}
 		}
 	}
 
-	if(x.size() != 0 && y.size() != 0 && t <= 41){
+	Pixel pixel;
+	double score = 0;
+
+	for (int x = 0; x < 900; x++){
+		for (int y = 0; y < 900; y++){
+			if(vote[x][y] > score){
+				score = vote[x][y];
+				
+				pixel.x = x;
+				pixel.y = y;
+			}
+		}
+	} 
+
+	draw_edge_and_center(pixel, edge, edge_count);
+    
+    cout<<score<<endl;
+	if(score < 25000){
+		pixel.x = -1000;
+		pixel.y = -1000;
+	}
+	return pixel;
+}
+
+//store position for orbit
+void store_orbit_position(int t){
+	Pixel pixel = vote_center();
+	int x = pixel.x;
+	int y = pixel.y;
+	
+	if(x > 0 && y > 0){
 		orbit.t.push_back(t);
-		orbit.x.push_back(x.at((int)x.size()/2));
-	 	orbit.y.push_back(y.at((int)y.size()/2));	
+		orbit.x.push_back(x);
+		orbit.y.push_back(y);
 	}
 }
 
@@ -233,14 +333,15 @@ int main(){
 	//int x_sun, y_sun; // current position of the sun
     for ( int time = 0 ; time < 950; time++){
 		cout<<endl;
-		filter_edge();
+
+		//vote_center();
+		//filter_edge();
 		if(time < 41) 		{store_orbit_position(time);}
 		else if(time == 41) {calculate_orbit();}
 		else{
-			go_to_sun();
 			get_sun_position_with_formula(time);
 		}
-
+		go_to_sun();
 
       	draw_all(time);  // image is ready, 
       	cout<<" time="<<time<<endl;
